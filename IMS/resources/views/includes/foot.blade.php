4a37a9d3d993e33ec3ca7e4471998974877abc2b
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-04 18:30:47 +0500

    removed foot into a new include

---
+++ IMS/resources/views/includes/foot.blade.php
@@ -0,0 +1,19 @@
+<!-- ================================================
+    Scripts
+    ================================================ -->
+    
+    <!-- jQuery Library -->
+    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/jquery-1.11.2.min.js"></script>    
+    <!--materialize js-->
+    <script type="text/javascript" src="{{url('assets/')}}/js//materialize.js"></script>
+    <!--prism-->
+    <script type="text/javascript" src="{{url('assets/')}}/js//prism/prism.js"></script>
+    <!--scrollbar-->
+    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <!-- chartist -->
+    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/chartist-js/chartist.min.js"></script>   
+    
+    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="{{url('assets/')}}/js//plugins.js"></script>
+    <!--custom-script.js - Add your own theme custom JS-->
+    <script type="text/javascript" src="{{url('assets/')}}/js//custom-script.js"></script>
