c66a43b4e64f724e34fae75d309407311c125c70
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-06 12:29:57 +0500

    added 404 view template

---
+++ IMS/resources/views/errors/404.blade.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head',['css'=>['layouts/page-center.css'],'pagetitle'=>'404'])
+
+<body class="cyan">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  <div id="error-page">
+
+    <div class="row">
+      <div class="col s12">
+        <div class="browser-window">
+          <div class="top-bar">
+            <div class="circles">
+              <div id="close-circle" class="circle"></div>
+              <div id="minimize-circle" class="circle"></div>
+              <div id="maximize-circle" class="circle"></div>
+            </div>
+          </div>
+          <div class="content">
+            <div class="row">
+              <div id="site-layout-example-top" class="col s12">
+                <p class="flat-text-logo center white-text caption-uppercase">Sorry but we couldn’t find this page :(</p>
+              </div>
+              <div id="site-layout-example-right" class="col s12 m12 l12">
+                <div class="row center">
+                  <h1 class="text-long-shadow col s12">404</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  @include('includes.foot')
+  
+  <script type="text/javascript">
+    function goBack() {
+      window.history.back();
+    }
+  </script>
+</body>
+
+</html>
