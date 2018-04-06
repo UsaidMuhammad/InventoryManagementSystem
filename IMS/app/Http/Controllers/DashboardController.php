dcd2addf8df555b3dbd1b0b26df1cbc5d88322a3
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-06 16:20:48 +0500

    get permission data of user and store it in session
    then forward it to the view

--- IMS/app/Http/Controllers/DashboardController.php
+++ IMS/app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Session\SessionManager;
 use Illuminate\Http\Request;
+use App\User;
 
 class DashboardController extends Controller
 {
@@ -13,9 +15,31 @@ class DashboardController extends Controller
     
     public function index()
     {
+        $this->addPermission();
         $data = [
             'pagetitle' => 'Dashboard',
+            'permission' => Session()->get('permission'),
+            'name' => \Auth::user()->name,
         ];
         return view('dashboard',$data);;
+    }
+
+    private function addPermission()
+    {
+        if (!Session()->has('permission')) {
+            $permission = User::find(\Auth::user()->id)->details;
+            $permission_arr = [
+                'customer' => $permission->permission_customer,
+                'supplier' => $permission->permission_supplier,
+                'product' => $permission->permission_product,
+                'stocks' => $permission->permission_stocks,
+                'sales' => $permission->permission_sales,
+                'payment' => $permission->permission_payment,
+                'report' => $permission->permission_report,
+                'users' => $permission->permission_users,
+            ];
+
+            Session()->put('permission', $permission_arr);
+        }
     }
 }
