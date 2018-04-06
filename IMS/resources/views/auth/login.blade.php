b5f223a55dd3647ff2122f8938b9876ca9982d5d
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-06 12:29:34 +0500

    edited new login template to be compatible with laravel login

--- IMS/resources/views/auth/login.blade.php
+++ IMS/resources/views/auth/login.blade.php
@@ -16,44 +16,60 @@
 
   <div id="login-page" class="row">
     <div class="col s12 z-depth-4 card-panel">
-      <form class="login-form">
+        {{ Form::open(['url'=>'/login','class'=>'login-form']) }}
         <div class="row">
           <div class="input-field col s12 center">
             <img src="{{url('assets')}}/images/login-logo.png" alt="" class="circle responsive-img valign profile-image-login">
-            <p class="center login-form-text">Material Design Admin Template</p>
+            <p class="center login-form-text">Login</p>
           </div>
         </div>
+        @if ($errors->has('email'))
+        <div id="card-alert" class="card red">
+            <div class="card-content white-text">
+              <p>{{ $errors->first('email') }}</p>
+            </div>
+        </div>
+        @endif
+        @if ($errors->has('password'))
+        <div id="card-alert" class="card red">
+            <div class="card-content white-text">
+              <p>{{ $errors->first('password') }}</p>
+            </div>
+        </div>
+        @endif
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-social-person-outline prefix"></i>
-            <input id="username" type="text">
-            <label for="username" class="center-align">Username</label>
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+            <label for="username" class="center-align">Email</label>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-action-lock-outline prefix"></i>
-            <input id="password" type="password">
+            <input id="password" type="password" class="form-control" name="password" required>
             <label for="password">Password</label>
           </div>
         </div>
         <div class="row">          
           <div class="input-field col s12 m12 l12  login-text">
-              <input type="checkbox" id="remember-me" />
+              <input type="checkbox" name="remember" id="remember-me" {{ old('remember') ? 'checked' : '' }}>
               <label for="remember-me">Remember me</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <a href="index.html" class="btn waves-effect waves-light col s12">Login</a>
+              <button type="submit" class="btn waves-effect waves-light col s12">
+                  Login
+              </button>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s6 m6 l6">
-            <p class="margin medium-small"><a href="page-register.html">Register Now!</a></p>
+            <p class="margin medium-small"><a href="{{route('register')}}">Register Now!</a></p>
           </div>
           <div class="input-field col s6 m6 l6">
-              <p class="margin right-align medium-small"><a href="page-forgot-password.html">Forgot password ?</a></p>
+              <p class="margin right-align medium-small"><a href="{{route('password.request')}}">Forgot password ?</a></p>
           </div>          
         </div>
 
