5701ac041d1e0235c0630cbceb493c2a8b8f9a22
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-06 14:58:01 +0500

    one to one relation with UserDetails

--- IMS/app/User.php
+++ IMS/app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function details()
+    {
+        return $this->hasOne('App\UserDetails','users_id');
+    }
 }
