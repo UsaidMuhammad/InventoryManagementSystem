6644107dd9677126913b03b243b79747116acc2d
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-13 15:28:16 +0500

    customer table migration

---
+++ IMS/database/migrations/2018_04_13_100124_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customername', 100);
+            $table->string('customernumber', 15);
+            $table->string('customeremail', 50);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
