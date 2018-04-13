6fa8b38638ad5302c84f7af98d782856af77c3e8
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-13 17:57:55 +0500

    footer include

---
+++ IMS/resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- START FOOTER -->
+<footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container center-align">
+        <span> Project by <a class="grey-text text-lighten-4" href="https://github.com/UsaidMuhammad">Muhammad Usaid</a></span>
+        </div>
+    </div>
+  </footer>
+  <!-- END FOOTER -->
