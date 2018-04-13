cd1157403ffde2a2aa9047660a820bcfc7ac5b21
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-13 17:57:26 +0500

    remove footer into a new file and include it

--- IMS/resources/views/dashboard.blade.php
+++ IMS/resources/views/dashboard.blade.php
@@ -70,21 +70,9 @@
   </div>
   <!-- END MAIN -->
 
-
+  @include('includes.footer')
 
   <!-- //////////////////////////////////////////////////////////////////////////// -->
-
-  <!-- START FOOTER -->
-  <footer class="page-footer">
-    <div class="footer-copyright">
-      <div class="container center-align">
-        <span> Project by <a class="grey-text text-lighten-4" href="https://github.com/UsaidMuhammad">Muhammad Usaid</a></span>
-        </div>
-    </div>
-  </footer>
-  <!-- END FOOTER -->
-
-
 
     @include('includes.foot')
     
