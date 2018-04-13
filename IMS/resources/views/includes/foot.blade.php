bfbeba84495fa5aeceb117b2be91b3149e57803b
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-13 17:57:28 +0500

    various changes to foot include code for custom js files

--- IMS/resources/views/includes/foot.blade.php
+++ IMS/resources/views/includes/foot.blade.php
@@ -3,17 +3,23 @@
     ================================================ -->
     
     <!-- jQuery Library -->
-    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/jquery-1.11.2.min.js"></script>    
+    <script type="text/javascript" src="{{url('assets/')}}/js/plugins/jquery-1.11.2.min.js"></script>    
+    @if (isset($js))
+        @foreach ($js as $item)
+        <script type="text/javascript" src="{{url('assets/')}}/js/{{$item}}"></script>
+        @endforeach
+    @endif
     <!--materialize js-->
-    <script type="text/javascript" src="{{url('assets/')}}/js//materialize.js"></script>
+    <script type="text/javascript" src="{{url('assets/')}}/js/materialize.min.js"></script>
     <!--prism-->
-    <script type="text/javascript" src="{{url('assets/')}}/js//prism/prism.js"></script>
+    <script type="text/javascript" src="{{url('assets/')}}/js/plugins/prism/prism.js"></script>
     <!--scrollbar-->
-    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script type="text/javascript" src="{{url('assets/')}}/js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
     <!-- chartist -->
-    <script type="text/javascript" src="{{url('assets/')}}/js//plugins/chartist-js/chartist.min.js"></script>   
+    <script type="text/javascript" src="{{url('assets/')}}/js/plugins/chartist-js/chartist.min.js"></script>   
     
     <!--plugins.js - Some Specific JS codes for Plugin Settings-->
-    <script type="text/javascript" src="{{url('assets/')}}/js//plugins.js"></script>
+    <script type="text/javascript" src="{{url('assets/')}}/js/plugins.min.js"></script>
     <!--custom-script.js - Add your own theme custom JS-->
-    <script type="text/javascript" src="{{url('assets/')}}/js//custom-script.js"></script>
+    <script type="text/javascript" src="{{url('assets/')}}/js/custom-script.js"></script>
+    
