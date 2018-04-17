f841f717558b68a5557ef4962d7836d2da6fff00
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-17 17:35:42 +0500

    supplier view and model setup

---
+++ IMS/resources/views/supplier/create.blade.php
@@ -0,0 +1,175 @@
+<?php //dd($customers)?>
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+    <div id="loader"></div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  @include('includes.header')
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+      @include('includes.leftnav')
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+      <!-- START CONTENT -->
+      <section id="content">
+
+        <!--breadcrumbs start-->
+        <div id="breadcrumbs-wrapper">
+          <!-- Search for small screen -->
+          <div class="header-search-wrapper grey hide-on-large-only">
+            <i class="mdi-action-search active"></i>
+            <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Explore Materialize">
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title">{{ $pagetitle }}</h5>
+                <ol class="breadcrumbs">
+                  <li>
+                    <a href="{{url('/home')}}">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="{{url('/customers')}}">Customers</a>
+                  </li>
+                  @if (isset($customer_edit))
+                    <li>
+                      <a href="{{url('/customers'.$customer_edit->id.'/create')}}">Edit</a>
+                    </li>  
+                  @else
+                    <li>
+                      <a href="{{url('/customers/create')}}">Add new</a>
+                    </li>
+                  @endif
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+
+        <!-- Form with validation for add a new Customer-->
+        <div class="col s12 m12 l6">
+          <div class="card-panel">
+            <h4 class="header2">Add a New Customer</h4>
+            @if ($errors->any())
+            <div id="card-alert" class="card red">
+                <div class="card-content white-text">
+                  @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                  @endforeach
+                </div>
+            </div>
+            @endif
+            @if (isset($customer_edit))
+            {{Form::open(['url'=>'/customers/'.$customer_edit->id,'class'=>'formValidate','id'=>'formValidate','method'=>'PUT'])}}
+            @else
+            {{Form::open(['url'=>'/customers','class'=>'formValidate','id'=>'formValidate'])}}
+            @endif
+            <div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="mdi-action-account-circle prefix"></i>
+                      <input id="name" name="name" type="text" class="validate" value="{{isset($customer_edit) ? $customer_edit->customer_name : null }}">
+                    <label for="name">Name</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="mdi-communication-email prefix"></i>
+                    <input id="email" type="email" name="email" class="validate" value="{{isset($customer_edit) ? $customer_edit->customer_email : null }}">
+                    <label for="email">Email</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="mdi-action-settings-phone prefix"></i>
+                    <input id="number" name="number" type="text" class="validate" value="{{isset($customer_edit) ? $customer_edit->customer_number : null }}">
+                    <label for="number">Number</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <p>Status ?</p>
+                  <p>
+                    <input name="status" type="radio" id="enable" value="1" {{isset($customer_edit) && $customer_edit->status == 1 ? 'checked' : null}}/>
+                    <label for="enable">enable</label>
+                  </p>
+                  <p>
+                    <input name="status" type="radio" id="disable" value="0" {{isset($customer_edit) && $customer_edit->status == 0 ? 'checked' : null}}/>
+                    <label for="disable">disable</label>
+                  </p>
+                </div>
+                <div class="input-field col s12">
+                    <button class="btn waves-effect waves-light right submit" type="submit" name="submit">Submit
+                      <i class="mdi-content-send right"></i>
+                    </button>
+                </div>
+            </div>
+            </form>
+          </div>
+        </div>
+    </div>
+
+
+    </section>
+    <!-- END CONTENT -->
+
+  </div>
+  <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+  @include('includes.footer') @include('includes.foot')
+
+  <script>
+    $("#formValidate").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        number: {
+          required: true,
+          digits: true
+        }
+      },
+      errorElement: 'div',
+      errorPlacement: function (error, element) {
+        var placement = $(element).data('error');
+        if (placement) {
+          $(placement).append(error)
+        } else {
+          error.insertAfter(element);
+        }
+      }
+    });
+  </script>
+</body>
+
+</html>
---
+++ IMS/resources/views/supplier/index.blade.php
@@ -0,0 +1,167 @@
+<?php //dd($suppliers)?>
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  @include('includes.header')
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+      @include('includes.leftnav')
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+      <!-- START CONTENT -->
+      <section id="content">
+        
+        <!--breadcrumbs start-->
+        <div id="breadcrumbs-wrapper">
+            <!-- Search for small screen -->
+            <div class="header-search-wrapper grey hide-on-large-only">
+                <i class="mdi-action-search active"></i>
+                <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Explore Materialize">
+            </div>
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title">{{ $pagetitle }}</h5>
+                <ol class="breadcrumbs">
+                  <li><a href="{{url('/home')}}">Dashboard</a></li>
+                  <li><a href="{{url($pagetitle)}}">{{$pagetitle}}</a></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+        
+
+        <!--start container-->
+        <div class="container">
+          <div class="section">
+            
+            <!--DataTables example-->
+            <div id="table-datatables">
+            <a href="/supplier/create" class="btn waves-effect waves-light yellow left ">Add New</a>
+            {{Form::open(["url"=>"/supplier/delete","method"=>"DELETE"])}}
+            <input type="submit" value="delete" class="btn waves-effect waves-light red right">
+              <div class="row">
+                <div class="col s12 m12 l12">
+                  <table id="data-table-simple" class="responsive-table display" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th><input type="checkbox" id="test1" class="parent_tick"><label for="test1"></label></th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Number</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Created</th>
+                            <th>Updated</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                 
+                    <tfoot>
+                        <tr>
+                            <th><input type="checkbox" id="test2" class="parent_tick"><label for="test2"></label></th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Number</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Created</th>
+                            <th>Updated</th>
+                            <th>Edit</th>
+                        </tr>
+                    </tfoot>
+                 
+                    <tbody>
+                        @foreach ($suppliers as $supplier)
+                        <tr>
+                            <td><input type="checkbox" id="{{$supplier->id}}" name="id[]" value ="{{$supplier->id}}"class="tick"><label for="{{$supplier->id}}"></label></td>
+                            <td>{{$supplier->name}}</td>
+                            <td>{{$supplier->address}}</td>
+                            <td>{{$supplier->number}}</td>
+                            <td>{{$supplier->email}}</td>
+                            <td>{{$supplier->status}}</td>
+                            <td>{{$supplier->created_at}}</td>
+                            <td>{{$supplier->updated_at}}</td>
+                            <td><a href="{{url('supplier/'.$supplier->id.'/edit')}}" class="btn waves-effect waves-light blue">Edit</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                  </table>
+                  </form>
+                </div>
+              </div>
+            </div> 
+            <br>
+          </div>
+        </div>
+        <!--end container-->
+      </section>
+      <!-- END CONTENT -->
+
+    </div>
+    <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+  @include('includes.footer')
+    @include('includes.foot')
+    <script type="text/javascript">
+        /*Show entries on click hide*/
+        $(document).ready(function(){
+            $(".dropdown-content.select-dropdown li").on( "click", function() {
+                var that = this;
+                setTimeout(function(){
+                if($(that).parent().hasClass('active')){
+                        $(that).parent().removeClass('active');
+                        $(that).parent().hide();
+                }
+                },100);
+            });
+
+            //select all checkboxes
+            $(".parent_tick").change(function(){  //"select all" change 
+            $(".tick").prop('checked', $(this).prop("checked")); //change all ".tick" checked status
+            });
+
+            //".tick" change 
+            $('.tick').change(function(){ 
+            //uncheck "select all", if one of the listed checkbox item is unchecked
+            if(false == $(this).prop("checked")){ //if this item is unchecked
+                $(".parent_tick").prop('checked', false); //change "select all" checked status to false
+            }
+            //check "select all" if all checkbox items are checked
+            if ($('.tick:checked').length == $('.tick').length ){
+                $(".parent_tick").prop('checked', true);
+            }
+            });
+        });
+    </script>
+</body>
+
+</html>
