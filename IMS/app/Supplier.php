be126cf2037ca99093ae090d757a47d9fa05298c
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-04-20 08:06:32 +0500

    product has one supplier

--- IMS/app/Supplier.php
+++ IMS/app/Supplier.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     protected $table = 'supplier';
+
+    /**
+     * Get the supplier for the products.
+     */
+    public function supplier()
+    {
+        return $this->hasOne('App\Supplier');
+    }
 }
